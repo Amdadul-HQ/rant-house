5a3b804d67d04c9fa1a789a590333bae3ec4d5f3
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-08-22 21:46:34 +0600

    adding dynamic routes

---
+++ src/app/shop/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+           <h1>Hello from shops</h1> 
+        </div>
+    );
+};
+
+export default Shop;
---
+++ src/app/sublet/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sublet = () => {
+    return (
+        <div>
+            <h1>Hello from Sublet</h1>
+        </div>
+    );
+};
+
+export default Sublet;
