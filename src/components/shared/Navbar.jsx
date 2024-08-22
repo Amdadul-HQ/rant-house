13b76f52c1bbcf84a314be9773a7c80991431c18
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-08-22 21:12:10 +0600

    adding navbar for desktop

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -25,13 +25,13 @@ const Navbar = () => {
     ]
     return (
         <section>
-            <nav className="max-w-screen-xl mx-auto py-4 flex justify-between">
-                <h1>
+            <nav className="max-w-screen-xl mx-auto py-4 flex justify-between bg-white px-7 mt-4 rounded-xl items-center shadow-slate-200 shadow-[0px_0px_10px]">
+                <h1 className="text-3xl font-semibold">
                     BASA BARA
                 </h1>
-                <div>
-                    <Button text={'Contact'} className={'bg-black text-white'}/>
-                    
+                <div className="flex gap-x-2">
+                    <Button text={'Contact'} className={'bg-black text-white border-2 border-black'}/>
+                    <Button text={'Loging'} className={'bg-white border-2 border-black text-black'}/>
                 </div>
             </nav>
         </section>
