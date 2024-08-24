c68fcc4ef1ff11f7e263e6da661ae013cc8cb003
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-08-24 21:11:12 +0600

    adding select option

--- src/components/shared/FilterSection.jsx
+++ src/components/shared/FilterSection.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const FilterSection = () => {
     return (
         <div>
-            <h1>Hello from filter</h1>
+            <select>
+                <option value="">Sort by</option>
+                <option value="price-high">Price (High to Low)</option>
+                <option value="price-low">Price (Low to High)</option>
+                <option value="bedrooms">Bedrooms (Low to High)</option>
+                <option value="bathrooms">Bathrooms (Low to High)</option>
+                <option value="square-feet">Square Feet (Low to High)</option>
+            </select>
         </div>
     );
 };
